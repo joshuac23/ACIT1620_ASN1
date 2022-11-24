900106ec739b7dc6e569eb9b67049398c421c5a8
Joshua <josh_jay23@hotmail.com>
2022-11-23 22:36:29 -0800

    add functionality to cancel and new note button. Cancel buttom removes text box and save/cancel buttons. New note button clears text area and restores the buttons

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,13 @@ const darkModeButton = document.querySelector(".dark_theme");
 const notes = document.querySelector(".notes");
 const textarea = document.querySelector("textarea");
 
+
+const textbox = document.querySelector(".text_container")
+const cancelButton = document.querySelector(".cancel");
+const saveButton = document.querySelector(".save")
+
+const newNoteButton = document.querySelector(".new_note")
+
 function darkMode () {
     document.body.classList.toggle("dark_mode");
     notes.classList.toggle("dark_mode2");
@@ -17,3 +24,23 @@ function darkMode () {
 }
 
 darkModeButton.addEventListener("click", darkMode)
+
+
+// creating cancel button functionality
+function hideText () {
+    textbox.classList.toggle("hidden"); 
+    saveButton.classList.toggle("hidden");
+    cancelButton.classList.toggle("hidden");
+}
+
+cancelButton.addEventListener("click", hideText)
+
+
+function newNote () {
+    textbox.classList.remove("hidden");
+    textarea.value = ''; // clear text in textbox
+    saveButton.classList.remove("hidden");
+    cancelButton.classList.remove("hidden");
+}
+
+newNoteButton.addEventListener("click", newNote)
